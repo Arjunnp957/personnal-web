932498e6bbfeb42a8528ead1716d926bd1cf04b3
Arjunnp957 <ajuarjun168@gmail.com>
2026-08-05 20:32:48 +0530

    Update settings.py

--- backend/portifolio_api/settings.py
+++ backend/portifolio_api/settings.py
@@ -80,17 +80,14 @@ WSGI_APPLICATION = 'portifolio_api.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
-    }
-}
+import dj_database_url
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL'),
+        conn_max_age=600,
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
